5a1f07fe8619076e53f4c77c18ac5346efa9bd22
Ahmed <adahmani010@gmail.com>
2022-02-09 21:34:31 +0100

    Fixed api links with the key

--- src/api.js
+++ src/api.js
@@ -29,16 +29,16 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10&key=${process.env.REACT_APP_RAWG_API_KEY}`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10&key=${process.env.REACT_APP_RAWG_API_KEY}`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10&key=${process.env.REACT_APP_RAWG_API_KEY}`;
 
 export const popularGamesURL = () =>
-  `${base_url}${popular_games}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+  `${base_url}${popular_games}`;
 export const upcomingGamesURL = () =>
-  `${base_url}${upcoming_games}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+  `${base_url}${upcoming_games}`;
 export const newGamesURL = () =>
-  `${base_url}${newGames}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+  `${base_url}${newGames}`;
 //GAME DETAILS
 export const gameDetailsURL = (game_id) =>
   `${base_url}games/${game_id}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
