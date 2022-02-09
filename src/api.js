f76a3b32761196f9f3b2cd3423e2050b02f797de
Ahmed <adahmani010@gmail.com>
2022-02-09 02:56:38 +0100

    Added api.js with apiKey in links

---
+++ src/api.js
@@ -0,0 +1,50 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+
+//Getting the date
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+//Getting the date
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+//Current day/month/year
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
+export const popularGamesURL = () =>
+  `${base_url}${popular_games}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+export const upcomingGamesURL = () =>
+  `${base_url}${upcoming_games}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+export const newGamesURL = () =>
+  `${base_url}${newGames}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+//GAME DETAILS
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+//Game ScreenShots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_RAWG_API_KEY}`;
+//Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9&key=${process.env.REACT_APP_RAWG_API_KEY}`;
