454b38e1a60cc62c66f3dc95dfe5e36fbdd097e7
Ahmed <adahmani010@gmail.com>
2022-02-16 23:30:33 +0100

    Added game component and use it in Home page with some styles

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,13 +1,19 @@
 const initState = {
-  games: [],
   popular: [],
+  newGames: [],
   upcoming: [],
+  searched: [],
 };
 
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
     default:
       return { ...state };
   }
